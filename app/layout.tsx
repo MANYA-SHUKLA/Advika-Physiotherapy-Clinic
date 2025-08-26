da3706f8184c358142d0873e1aa3acbf51e02de8
MANYA SHUKLA <shuklamanya99@gmail.com>
2025-08-27 02:18:17 +0530

    New good updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import Script from "next/script";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
 export const metadata = {
   title: "Physiotherapy Clinic",
   description: "Resurs Clone with Next.js + Tailwind",
@@ -12,8 +11,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-
-        {/* ✅ Navbar & Footer wrap every page */}
         <Navbar />
         {children}
         <Footer />
